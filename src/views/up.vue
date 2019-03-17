7cdd4d0c0c892af643e1624b8d588732861f6142
stupidfish <2928109164>
2019-03-17 12:01:16 +0800

    add

--- src/views/up.vue
+++ src/views/up.vue
@@ -1,71 +1,84 @@
 <template>
 	<div>
-	<headTop></headTop>
-<div class="upmain" onclick="uploade()">
+		<headTop></headTop>
+		<div class="upmain" onclick="uploade()">
 			<form id="_uploadForm" enctype="multipart/form-data"><input id="file" type="file" accept=".zip" />
-			<textarea class="restitle" placeholder="输入一下简介叭"></textarea>
-			<input type="radio" class="resradio"/>
+				<textarea class="restitle" placeholder="输入一下简介叭"></textarea>
+				<input type="radio" class="resradio" />
 			</form>
 			<br/><br/>
 			<p id="url">点击加号上传</p>
 		</div>
+			<input type="submit" value="提交" class="submitres" />
+		<endLine></endLine>
 
-<endLine></endLine>
-	
 	</div>
 </template>
 
-	<script >
+<script>
+	import headTop from '@/components/headTop.vue'
+	import endLine from '@/components/endLine.vue'
+	export default {
+		name: 'up',
+		components: {
+			headTop,
+			endLine
+		}
+	}
+	var file = $('#file'),
+		aim = $('#url');
+	file.on('change', function(e) {
+		//e.currentTarget.files 是一个数组，如果支持多个文件，则需要遍历
+		var name = e.currentTarget.files[0].name;
+		aim.text(name);
+	});
+</script>
+<style type="text/css">
+	.submitres {
+		position: absolute;
+		background: initial;
+		color: #FFFFFF;
+		font-size: 30px;
+		left:0px;
+	}
+	#join:hover {
+		border-top: 7px solid rgb(3, 35, 14);
+	}
 	
-		import headTop from '@/components/headTop.vue'
-		import endLine from '@/components/endLine.vue'
-		export default {
-  name: 'up',
-  components: {
-    headTop,
-    endLine
-  }
-}
+	#_uploadForm {}
 	
-	</script>
-	<style type="text/css">
-		#join:hover {
-				border-top: 7px solid rgb(3, 35, 14);
-			}
-			 
-			#_uploadForm {}
-			#file:hover{
-				opacity: 0.6;
-			}
-			
-			#file {
-				position: absolute;
-				width: 400px;
-				height: 400px;
-				margin: auto;
-				top: 20%;
-				left: 0;
-				right: 0;
-				bottom: 0;
-				text-align: center;
-				filter: blur(10px);
-				filter: brightness(150%);
-				background-color: #FFFFFF;
-				opacity: 0.2;
-				background-image: url(../assets/img/add.png);
-				background-size: 100%;
-				background-repeat: no-repeat;
-				border-radius: 1000px;
-			}
-			
-			#url {
-				color: coral;
-				font-size: 30px;
-			}
-			.restitle{
-				position: absolute;
-				width: 200px;
-				height: 200px;
-				
-			}
-	</style>
+	#file:hover {
+		opacity: 0.6;
+	}
+	
+	#file {
+		position: absolute;
+		width: 400px;
+		height: 400px;
+		margin: auto;
+		top: 20%;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		text-align: center;
+		filter: blur(10px);
+		filter: brightness(150%);
+		background-color: #FFFFFF;
+		opacity: 0.2;
+		background-image: url(../assets/img/add.png);
+		background-size: 100%;
+		background-repeat: no-repeat;
+		border-radius: 1000px;
+	}
+	
+	#url {
+		color: coral;
+		font-size: 30px;
+	}
+	
+	.restitle {
+		position: absolute;
+		width: 200px;
+		height: 200px;
+	}
+</style>
