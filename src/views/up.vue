4880031b174867424c43641d4182423d82ec5666
stupidfish <2928109164>
2019-03-17 17:25:51 +0800

    2019-03-17 17:25

--- src/views/up.vue
+++ src/views/up.vue
@@ -1,62 +1,72 @@
 <template>
-	<div >
-		<!-- 我疯了，到底咋回事
-		 加了:id="" 元素就会消失
-		 加了js部分，整个页面除了背景全消失
-		 你看看咋回事
+	<div>
+		<!-- ok
 		 -->
 		<headTop></headTop>
 		<div class="upmain" onclick="uploade()">
-			<form id="_uploadForm" enctype="multipart/form-data"><input  id="file"  v-on:change= "success()" type="file" accept=".zip" />
-				<textarea class="restitle" placeholder="输入一下简介叭"></textarea>
-				<input type="radio" class="resradio" />
+			<form id="_uploadForm" enctype="multipart/form-data">
+				<input id="file" v-on:change="success()" type="file" accept=".zip" />
+				<textarea class="restitle" placeholder="输入一下简介叭" maxlength="233"></textarea>
+				<select class="resclass">
+					<option value="0">PVP</option>
+					<option value="1">生存</option>
+					<option value="2">高分辨率</option>
+					<option value="3">音效</option>
+					<option value="4">3D</option>
+					<option value="5">恶搞</option>
+					<option value="6">自制</option>
+				</select>
 			</form>
-			<br/><br/>
+			<br />
+			<br />
 			<p id="url">点击加号上传</p>
 		</div>
-			<input type="submit" value="提交" class="submitres" />
-		<endLine></endLine>
+		<input type="submit" value="提交" class="submitres" />
 
+		<endLine></endLine>
 	</div>
 </template>
 <script>
-		
-	
-	
-		import $ from 'jquery'
-	import headTop from '@/components/headTop.vue'
-	import endLine from '@/components/endLine.vue'
-	export default {
-		name: 'up',
-		components: {
-			headTop,
-			endLine,
-		},
-		mounted(){
+import $ from 'jquery';
+import headTop from '@/components/headTop.vue';
+import endLine from '@/components/endLine.vue';
+export default {
+	name: 'up',
+	components: {
+		headTop,
+		endLine
+	},
+	mounted() {
+		$('.restitle').hide();
+		$('.submitres').hide();
 
-			//这里写初始化的Jquery，只渲染一次
-			var file = $('#file'),
+		//这里写初始化的Jquery，只渲染一次
+		var file = $('#file'),
 			aim = $('#url');
 		file.on('change', function(e) {
 			//e.currentTarget.files 是一个数组，如果支持多个文件，则需要遍历
 			var name = e.currentTarget.files[0].name;
 			aim.text(name);
+			file.hide();
+			$('.restitle').css('top', '160px');
+			$('.restitle').css('left', '10%');
+			$('.restitle').css('width', '40%');
+			$('.restitle').css('height', '50%');
+			$('.restitle').css('height', '50%');
+			$('.restitle').show();
+			$('.submitres').show();
 		});
+	},
+	methods: {
+		//方法都写到这里
+		success: function(event) {},
 
-		},
-		methods:{
-			//方法都写到这里
-			success:function(event)
-			{
-				alert('上传成功');
-			},
-
-		 postData:function() {
+		postData: function() {
 			var formData = new FormData();
-			formData.append("file", $("#file")[0].files[0]);
-			formData.append("restitle",$("#restitle").val());
-			formData.append("service", 'App.Passion.UploadFile');
-			formData.append("token", token);
+			formData.append('file', $('#file')[0].files[0]);
+			formData.append('restitle', $('#restitle').val());
+			formData.append('service', 'App.Passion.UploadFile');
+			formData.append('token', token);
 			$.ajax({
 				url: 'http://stupidfish.ticp.net',
 				/*接口域名地址*/
@@ -66,69 +76,93 @@
 				processData: false,
 				success: function(res) {
 					console.log(res.data);
-					if(res.data["code"] == "succ") {
+					if (res.data['code'] == 'succ') {
 						alert('上传成功');
-					} else if(res.data["code"] == "err") {
+					} else if (res.data['code'] == 'err') {
 						alert('emmm上传失败');
 					} else {
 						console.log(res);
 					}
 				}
-			})
+			});
 		}
-		
-
-}
-	}  
-	
-	
+	}
+};
 </script>
 <style type="text/css">
-	.submitres {
-		position: absolute;
-		background: initial; 
-		color: #FFFFFF;
-		font-size: 30px;
-		left:0px;
-	}
-	#join:hover {
-		border-top: 7px solid rgb(3, 35, 14);
-	}
-	
-	#_uploadForm {}
-	
-	#file:hover {
-		opacity: 0.6;
-	}
-	
-	#file {
-		position: absolute;
-		width: 400px;
-		height: 400px;
-		margin: auto;
-		top: 20%;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		text-align: center;
-		filter: blur(10px);
-		filter: brightness(150%);
-		background-color: #FFFFFF;
-		opacity: 0.2;
-		background-image: url(../assets/img/add.png);
-		background-size: 100%;
-		background-repeat: no-repeat;
-		border-radius: 1000px;
-	}
-	
-	#url {
-		color: coral;
-		font-size: 30px;
-	}
-	
-	.restitle {
-		position: absolute;
-		width: 200px;
-		height: 200px;
-	}
+.resclass {
+	border-top: 8px solid #2b81af;
+	position: absolute;
+	top:160px;
+	left:800px;
+	color:#2b81af ;
+	font-size: 25px;
+	width:200px
+}
+.resclass:hover{
+	border: 2px solid #2b81af;
+}
+.restitle {
+	border-top: 0px solid #2b81af;
+	font-size: 25px;
+}
+.restitle:hover {
+	border-top: 40px solid #2b81af;
+}
+.restitle:focus {
+	border: 5px solid #528ce0;
+}
+.submitres {
+	position: absolute;
+	background: initial;
+	color: coral;
+	font-size: 30px;
+	border: 10px solid rgb(222, 125, 44);
+	left: 70.5rem;
+	bottom: 0px;
+}
+.submitres:hover {
+	border: 10px solid #528ce0;
+}
+#join:hover {
+	border-top: 7px solid rgb(3, 35, 14);
+}
+
+#_uploadForm {
+}
+
+#file:hover {
+	opacity: 0.6;
+}
+
+#file {
+	position: absolute;
+	width: 400px;
+	height: 400px;
+	margin: auto;
+	top: 20%;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	text-align: center;
+	filter: blur(10px);
+	filter: brightness(150%);
+	background-color: #ffffff;
+	opacity: 0.2;
+	background-image: url(../assets/img/add.png);
+	background-size: 100%;
+	background-repeat: no-repeat;
+	border-radius: 1000px;
+}
+
+#url {
+	color: coral;
+	font-size: 30px;
+}
+
+.restitle {
+	position: absolute;
+	width: 200px;
+	height: 200px;
+}
 </style>
