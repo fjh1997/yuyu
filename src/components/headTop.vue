89eab6f0dba33d230b9a75c15ee66addb45ede9a
stupidfish <2928109164>
2019-03-23 10:38:48 +0800

    2019-3-23 10:38

--- src/components/headTop.vue
+++ src/components/headTop.vue
@@ -7,7 +7,7 @@
 		
 	</head>
 
-	<body>
+	<body id="mydiv">
 		<div class="top"></div>
 		<div class="top_main">
 			<div class="class_res">
@@ -31,10 +31,32 @@
 </div>
 </template>
 <script>
-
 	
+	import {canvaspice} from '../../public/js/canvas-particle.js'
     export default {
     	name: 'HeadTop',
+		methods:{
+			methods1:function(){
+				window.onload = function() {
+						    //配置
+						    var config = {
+						        vx: 2,	//小球x轴速度,正为右，负为左
+						        vy: 2,	//小球y轴速度
+						        height: 0,	//小球高宽，其实为正方形，所以不宜太大
+						        width: 0,
+						        count: 200,		//点个数
+						        color: "121, 162, 185", 	//点颜色
+						        stroke: "200,101,200", 		//线条颜色
+						        dist: 15000,  //点吸附距离
+						        e_dist: 20000, 	//鼠标吸附加速距离
+						        max_conn: 15 	//点到点最大连接数
+						    }
+				
+						    //调用
+						    CanvasParticle(config);
+				}
+			}
+		}
     }
 </script>
 
@@ -45,11 +67,8 @@
 				-moz-transition: all 0.375s;
 				transition: all 0.375s;
 			}
-			
-			body {
-				background-image: url(../assets/img/background.jpg);
-				background-repeat: no-repeat;
-				background-size: 100%;
+			body{
+				background-color: #2D2D2D;
 			}
 			
 			.top {
@@ -90,7 +109,7 @@
 			}
 			.pushdatadiv{
 				position: page;
-				background-color: rgb(249,205,173);
+				background-color:rgb(222,211,140) ;
 				width: 49%;
 				height: 400px;
 				border: 1px solid black;
--- src/views/newone.vue
+++ src/views/newone.vue
@@ -32,7 +32,7 @@
 <script>
 import headTop from '@/components/headTop.vue';
 import endLine from '@/components/endLine.vue';
-
+import $ from '../../public/js/canvas-particle.js'
 export default {
 	name: 'new',
 	components: {
@@ -41,8 +41,24 @@ export default {
 	},
 	mounted() {
 		//这里面写初始化的Jquery，在生命周期的mount阶段才有用。
-
-		$('.div').text('服务器未响应或未连接服务器 \n 无资源'); //有内容删掉这个就行
+     window.onload = function() {
+     		    //配置
+     		    var config = {
+     		        vx: 2,	//小球x轴速度,正为右，负为左
+     		        vy: 2,	//小球y轴速度
+     		        height: 0,	//小球高宽，其实为正方形，所以不宜太大
+     		        width: 0,
+     		        count: 200,		//点个数
+     		        color: "121, 162, 185", 	//点颜色
+     		        stroke: "200,101,200", 		//线条颜色
+     		        dist: 15000,  //点吸附距离
+     		        e_dist: 20000, 	//鼠标吸附加速距离
+     		        max_conn: 15 	//点到点最大连接数
+     		    }
+     
+     		    //调用
+     		    CanvasParticle(config);
+     }
 	},
 	methods: {
 		//这里面写方法函数都没关系
