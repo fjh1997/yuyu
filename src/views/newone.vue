41b869bf774adb8e5d6820c928c47c2bb883e543
stupidfish <2928109164>
2019-03-23 23:00:48 +0800

    背景粒子更新

--- src/views/newone.vue
+++ src/views/newone.vue
@@ -9,7 +9,7 @@
 		<br />
 		<br />
 		<div class="maindiv">
-			<div class="pushdatadiv">
+			<div class="pushdatadiv" >
 				<img src="#" style="width: 100%; height: 250px; background: white;" />
 				<hr style="height:1px;border:none;border-top:1px dashed #0066CC;" />
 			</div>
@@ -41,24 +41,7 @@ export default {
 	},
 	mounted() {
 		//这里面写初始化的Jquery，在生命周期的mount阶段才有用。
-     window.onload = function() {
-     		    //配置
-     		    var config = {
-     		        vx: 2,	//小球x轴速度,正为右，负为左
-     		        vy: 2,	//小球y轴速度
-     		        height: 0,	//小球高宽，其实为正方形，所以不宜太大
-     		        width: 0,
-     		        count: 200,		//点个数
-     		        color: "121, 162, 185", 	//点颜色
-     		        stroke: "200,101,200", 		//线条颜色
-     		        dist: 15000,  //点吸附距离
-     		        e_dist: 20000, 	//鼠标吸附加速距离
-     		        max_conn: 15 	//点到点最大连接数
-     		    }
-     
-     		    //调用
-     		    CanvasParticle(config);
-     }
+
 	},
 	methods: {
 		//这里面写方法函数都没关系
